644a2d7341b50e14cf9bb5a8eb8d36b4578956af
Ugo Chiori <ugochiori@gmail.com>
2023-09-08 16:17:13 +0100

    time stamp

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+// // Declare variables for the day of the week and UTC time
+// const daysOfWeek = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
+//   const dayOfWeekElement = document.getElementById("dayOfWeek");
+//   const utcTimeElement = document.getElementById("utcTime");
+  
+//   // Function to format a timestamp as a human-readable string
+//   function formatTime(timestamp) {
+//     const date = new Date(timestamp);
+//     const hours = date.getUTCHours().toString().padStart(2, '0');
+//     const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+//     const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+  
+//     return `${hours}:${minutes}:${seconds} GMT +1`;
+//   }
+  
+//   // Function to update the current day of the week and UTC time
+//   function updateDateTime() {
+//     const now = new Date();
+//     const dayOfWeek = daysOfWeek[now.getUTCDay()];
+    
+//     // Adjust the timestamp for UTC
+//     now.setHours(now.getUTCHours() + 2);
+  
+//     const utcTime = formatTime(now.getTime());
+  
+//     dayOfWeekElement.textContent = ` ${dayOfWeek}`;
+//     utcTimeElement.textContent = ` ${utcTime}`;
+//   }
+  
+//   // Update the date and time immediately and then every second
+//   updateDateTime();
+//   setInterval(updateDateTime, 1000);
+
+
 // Declare variables for the day of the week and UTC time
 const daysOfWeek = [
     "Sunday",
@@ -11,14 +53,16 @@ const daysOfWeek = [
   const dayOfWeekElement = document.getElementById("dayOfWeek");
   const utcTimeElement = document.getElementById("utcTime");
   
-  // Function to format a timestamp as a human-readable string
+  // Function to format a timestamp as a human-readable string in AM/PM format
   function formatTime(timestamp) {
     const date = new Date(timestamp);
-    const hours = date.getUTCHours().toString().padStart(2, '0');
+    const hours = date.getUTCHours();
     const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-    const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+    const amPm = hours >= 12 ? 'PM' : 'AM';
+    const formattedHours = (hours % 12) || 12; // Convert to 12-hour format
+    const formattedSeconds = date.getUTCSeconds().toString().padStart(2, '0');
   
-    return `${hours}:${minutes}:${seconds} GMT +1`;
+    return `${formattedHours}:${minutes} ${formattedSeconds} ${amPm} GMT +1`;
   }
   
   // Function to update the current day of the week and UTC time
@@ -26,8 +70,10 @@ const daysOfWeek = [
     const now = new Date();
     const dayOfWeek = daysOfWeek[now.getUTCDay()];
     
-    // Adjust the timestamp for UTC
-    now.setHours(now.getUTCHours() + 2);
+    // Adjust the timestamp for GMT +1 (1 hour ahead of UTC)
+    now.setHours(now.getHours() + 1);
+   
+    
   
     const utcTime = formatTime(now.getTime());
   
