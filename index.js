2085e35b2fa9bf0306fcab0c0d4cd92727da623d
Ugo Chiori <ugochiori@gmail.com>
2023-09-08 11:27:11 +0100

    typos corrected

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// JavaScript code to populste data
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// Declare variables for the day of the week and UTC time
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 const dayOfWeekElement = document.getElementById("dayOfWeek");
 const utcTimeElement = document.getElementById("utcTime");
 
 // Function to get the current day of the week and UTC time
 function updateDateTime() {
-    const now = new Date();
-    const dayOfWeek = daysOfWeek[now.getUTCDay()];
-    const utcTime = now.getTime();
+  const now = new Date();
+  const dayOfWeek = daysOfWeek[now.getUTCDay()];
+  const utcTime = now.getTime();
 
-    dayOfWeekElement.textContent = dayOfWeek;
-    utcTimeElement.textContent = utcTime;
+  dayOfWeekElement.textContent = dayOfWeek;
+  utcTimeElement.textContent = utcTime;
 }
 
 // Update the date and time immediately and then every second
